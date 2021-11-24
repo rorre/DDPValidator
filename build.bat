cd584e72f2773d0e87c27b898fb129a2b67e64f9
Rendy Arya Kemal <renrror@gmail.com>
2021-11-24 19:51:09 +0700

    Add files for pyinstaller build

---
+++ build.bat
@@ -0,0 +1,7 @@
+pyinstaller run.py  --noconfirm --log-level=WARN ^
+    --onefile ^
+    --name "DDPValidator" ^
+    --console ^
+    --icon "NONE" ^
+    --workpath ./build/cli ^
+    --distpath ./dist/
---
+++ run.py
@@ -0,0 +1,4 @@
+from ddp_validator.cli import cli
+
+if __name__ == "__main__":
+    cli()
